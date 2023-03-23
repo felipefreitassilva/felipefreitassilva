3e27fefc5b16e9f75196cc2006cefd1c8f850a28
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-03-23 13:49:58 -0300

    Improved Date Handling

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-function calcularIdade() {
-    const data = new Date()
-    const dia = parseInt(data.getDate())
-    const mes = parseInt(data.getMonth() + 1)
-    const ano = parseInt(data.getFullYear())
-    const diaN = 2
-    const mesN = 4
-    const anoN = 2003
-    let minhaIdade = ano - anoN
-    if (mes < mesN) {
-        minhaIdade--
-    } else if (mes == mesN) {
-        if (dia < diaN) {
-            minhaIdade--
-        }
-    }
-    document.getElementById('idade').innerHTML = minhaIdade
-
-    document.getElementById('xp').innerHTML = ano - 2020
-}
-calcularIdade()
+const CURRENT_YEAR = new Date().getFullYear()
+const YEAR_STUDYING_BEGAN = 2021
+document.getElementById('xp').innerHTML = CURRENT_YEAR - YEAR_STUDYING_BEGAN
