4b8043372f0621e691974fa4228d946d99a51454
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-05-12 11:13:50 -0300

    minimized js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,1 @@
-const CURRENT_YEAR = new Date().getFullYear()
-const YEAR_STUDYING_BEGAN = 2021
-document.getElementById('xp').innerHTML = CURRENT_YEAR - YEAR_STUDYING_BEGAN
+document.getElementById('xp').innerHTML=new Date().getFullYear()-2021
